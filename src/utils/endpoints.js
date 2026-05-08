7e2952e675578a5f89da383d5df264258ca32aa0
Khushal Sachdeva <68191677+CodeKhushal@users.noreply.github.com>
2026-05-09 01:06:26 +0530

    Update API endpoints to new backend version

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,6 +1,6 @@
-export const USER_API_END_POINT = "https://expense-tracker-backend-v2-3gpy.onrender.com/api/v1/user";
-export const EXPENSE_API_END_POINT = "https://expense-tracker-backend-v2-3gpy.onrender.com/api/v1/expense";
-export const AI_API_END_POINT = "https://expense-tracker-backend-v2-3gpy.onrender.com/api/v1/ai";
+export const USER_API_END_POINT = "https://expense-tracker-backend-v2-1.onrender.com/api/v1/user";
+export const EXPENSE_API_END_POINT = "https://expense-tracker-backend-v2-1.onrender.com/api/v1/expense";
+export const AI_API_END_POINT = "https://expense-tracker-backend-v2-1.onrender.com/api/v1/ai";
 
 // export const USER_API_END_POINT = "https://expense-tracker-backend-kjg2.onrender.com/api/v1/user";
 // export const EXPENSE_API_END_POINT = "https://expense-tracker-backend-kjg2.onrender.com/api/v1/expense";
